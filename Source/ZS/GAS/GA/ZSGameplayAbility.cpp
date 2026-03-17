740e47b2300bd258a2a3754a62bc0e1b4b3be76e
LEEJS256 <68937552+LEEJS256@users.noreply.github.com>
2026-03-17 16:45:20 +0900

    fix(GA) : base GA 수정

--- Source/ZS/GAS/GA/ZSGameplayAbility.cpp
+++ Source/ZS/GAS/GA/ZSGameplayAbility.cpp
@@ -3,3 +3,63 @@
 
 #include "GAS/GA/ZSGameplayAbility.h"
 
+#include "AbilitySystemComponent.h"
+
+UZSGameplayAbility::UZSGameplayAbility()
+{
+}
+
+void UZSGameplayAbility::ActivateAbility(const FGameplayAbilitySpecHandle Handle,
+                                         const FGameplayAbilityActorInfo* ActorInfo, const FGameplayAbilityActivationInfo ActivationInfo,
+                                         const FGameplayEventData* TriggerEventData)
+{
+	Super::ActivateAbility(Handle, ActorInfo, ActivationInfo, TriggerEventData);
+}
+
+void UZSGameplayAbility::EndAbility(const FGameplayAbilitySpecHandle Handle, const FGameplayAbilityActorInfo* ActorInfo,
+	const FGameplayAbilityActivationInfo ActivationInfo, bool bReplicateEndAbility, bool bWasCancelled)
+{
+	Super::EndAbility(Handle, ActorInfo, ActivationInfo, bReplicateEndAbility, bWasCancelled);
+}
+
+void UZSGameplayAbility::ApplyDamageFromTrace()
+{
+}
+
+void UZSGameplayAbility::OnStopAttackTrace_Server()
+{
+	//몽타주 1번에 여러 공격이 들어갈 때 데미지 빨리 처리 시 불리는 함수
+	ApplyDamageFromTrace();
+}
+
+void UZSGameplayAbility::AddAttackTypeToEffectSpec(FGameplayEffectSpec& Spec) const
+{
+	if (AttackTypeTag.IsValid())
+	{
+		Spec.DynamicGrantedTags.AddTag(AttackTypeTag);
+	}
+}
+
+void UZSGameplayAbility::ApplyHeatGE(int32 HeatIndex, UAbilitySystemComponent* SourceASC)
+{
+	TSubclassOf<UGameplayEffect> HeatEffectClass =
+	HeatGE[0];
+
+	if (HeatEffectClass)
+	{
+		FGameplayEffectContextHandle HeatContext =
+			SourceASC->MakeEffectContext();
+
+		HeatContext.AddSourceObject(this);
+
+		FGameplayEffectSpecHandle HeatSpecHandle =
+			SourceASC->MakeOutgoingSpec(HeatEffectClass, 1.f, HeatContext);
+
+		if (HeatSpecHandle.IsValid())
+		{
+			SourceASC->ApplyGameplayEffectSpecToSelf(
+				*HeatSpecHandle.Data.Get()
+			);
+		}
+	}
+}
--- Source/ZS/GAS/GA/ZSGameplayAbility.h
+++ Source/ZS/GAS/GA/ZSGameplayAbility.h
@@ -13,5 +13,40 @@ UCLASS()
 class ZS_API UZSGameplayAbility : public UGameplayAbility
 {
 	GENERATED_BODY()
+public:
+	UZSGameplayAbility();
+
+	virtual void ActivateAbility(
+	const FGameplayAbilitySpecHandle Handle,
+	const FGameplayAbilityActorInfo* ActorInfo,
+	const FGameplayAbilityActivationInfo ActivationInfo,
+	const FGameplayEventData* TriggerEventData) override;
+
+	virtual void EndAbility(
+		const FGameplayAbilitySpecHandle Handle,
+		const FGameplayAbilityActorInfo* ActorInfo,
+		const FGameplayAbilityActivationInfo ActivationInfo,
+		bool bReplicateEndAbility,
+		bool bWasCancelled
+	) override;
+
+	virtual void ApplyDamageFromTrace();
+
 	
+	virtual void OnStopAttackTrace_Server();
+
+protected:
+	/** Damage Spec 생성 시 공격 타입 태그를 넣어준다 */
+	void AddAttackTypeToEffectSpec(FGameplayEffectSpec& Spec) const;
+
+	// void ApplyMotionWarp(AZSPlayerCharacter* SKPlayer);
+	// FVector GetAttackTargetLocation() const;
+	
+	UPROPERTY(EditAnywhere, Category = "SK|GAS")
+	TArray<TSubclassOf<UGameplayEffect>> HeatGE;
+
+	void ApplyHeatGE(int32 HeatIndex,UAbilitySystemComponent* SourceASC);
+	
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category="Attack")
+	FGameplayTag AttackTypeTag;
 };
