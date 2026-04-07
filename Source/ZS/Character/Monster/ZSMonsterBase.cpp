e9f618682353f03b2d0972c8792cee24327e6a75
LEEJS256 <68937552+LEEJS256@users.noreply.github.com>
2026-04-08 04:16:59 +0900

    fix(Monster) :죽음관련 처리

--- Source/ZS/Character/Monster/ZSMonsterBase.cpp
+++ Source/ZS/Character/Monster/ZSMonsterBase.cpp
@@ -2,8 +2,13 @@
 
 
 #include "Character/Monster/ZSMonsterBase.h"
+#include "Net/UnrealNetwork.h"
+#include "Components/CapsuleComponent.h"
+#include "DA/ZSPlayerDataAsset.h"
+#include "Components/WidgetComponent.h"
 #include "Controller/ZSAIController.h"
 #include "GAS/Attribute/ZSAttributeSet.h"
+#include "UI/ZS_MonsterHealthWidget.h"
 #include "Utility/ZSNativeGameplayTag.h"
 
 // Sets default values
@@ -17,7 +22,21 @@ AZSMonsterBase::AZSMonsterBase()
 
 	AIControllerClass = AZSAIController::StaticClass();
 	AutoPossessAI = EAutoPossessAI::PlacedInWorldOrSpawned;
-	
+
+	HealthBarComponent = CreateDefaultSubobject<UWidgetComponent>(TEXT("HealthBar"));
+
+	HealthBarComponent->SetupAttachment(RootComponent);
+
+	// GetCapsuleComponent()->SetCollisionEnabled(ECollisionEnabled::QueryAndPhysics);
+	// GetCapsuleComponent()->SetCollisionObjectType(ECC_Pawn);
+	// GetCapsuleComponent()->SetCollisionResponseToAllChannels(ECR_Ignore);
+	// GetCapsuleComponent()->SetCollisionResponseToChannel(ECC_WorldDynamic, ECR_Overlap);
+
+
+	GetCapsuleComponent()->SetCollisionEnabled(ECollisionEnabled::QueryAndPhysics);
+	GetCapsuleComponent()->SetCollisionObjectType(ECC_Pawn);
+	GetCapsuleComponent()->SetCollisionResponseToAllChannels(ECR_Block);
+	GetCapsuleComponent()->SetCollisionResponseToChannel(ECC_WorldDynamic, ECR_Overlap);
 }
 
 // Called when the game starts or when spawned
@@ -29,16 +48,33 @@ void AZSMonsterBase::BeginPlay()
 	{
 		AbilitySystemComponent->InitAbilityActorInfo(this, this);
 		AbilitySystemComponent->AddLooseGameplayTag(TAG_Team_Monster);
+		InitializePlayerDA();
 	}
 
-	MoveToNextPoint();
+	BaseDelegate();
+	BaseUI();
+	// MoveToNextPoint();
+}
 
+
+void AZSMonsterBase::GetLifetimeReplicatedProps(TArray<FLifetimeProperty>& OutLifetimeProps) const
+{
+	Super::GetLifetimeReplicatedProps(OutLifetimeProps);
+
+	// 예시
+	// DOREPLIFETIME(AZSMonsterBase, Health);
 }
 
 // Called every frame
 void AZSMonsterBase::Tick(float DeltaTime)
 {
 	Super::Tick(DeltaTime);
+
+	if (bIsDissolving)
+	{
+		UpdateDissolve(DeltaTime);
+	}
+
 }
 
 // Called to bind functionality to input
@@ -47,9 +83,30 @@ void AZSMonsterBase::SetupPlayerInputComponent(UInputComponent* PlayerInputCompo
 	Super::SetupPlayerInputComponent(PlayerInputComponent);
 }
 
+
 UAbilitySystemComponent* AZSMonsterBase::GetAbilitySystemComponent() const
 {
 	return AbilitySystemComponent;
+}
+
+void AZSMonsterBase::DeathMonster()
+{
+	if (AAIController* AI = Cast<AAIController>(GetController()))
+	{
+		AI->StopMovement();
+	}
+
+	// 충돌 끄기
+	GetCapsuleComponent()->SetCollisionEnabled(ECollisionEnabled::NoCollision);
+
+	// 메시 충돌도 끄는 게 안전
+	GetMesh()->SetCollisionEnabled(ECollisionEnabled::NoCollision);
+
+	// 잠깐 남겼다가 삭제 (연출용)
+	// SetLifeSpan(3.f);
+	ApplyDissolveMI();
+	SetDissolve();
+	// Destroy();
 }
 
 void AZSMonsterBase::MoveToNextPoint()
@@ -79,3 +136,211 @@ void AZSMonsterBase::Set_EndTargetPoint(ATargetPoint* ParaTarget)
 	EndTargetPoint = ParaTarget;
 }
 
+void AZSMonsterBase::UpdateHealthBarRotation()
+{
+	if (!HealthBarComponent || !HealthBarComponent->IsVisible())
+		return;
+
+	APlayerController* PC = GetWorld()->GetFirstPlayerController();
+	if (!PC) return;
+
+	FVector CameraLocation;
+	FRotator CameraRotation;
+	PC->GetPlayerViewPoint(CameraLocation, CameraRotation);
+
+	FVector WidgetLocation = HealthBarComponent->GetComponentLocation();
+
+	// 1 카메라 바라보기
+	FVector Direction = CameraLocation - WidgetLocation;
+	FRotator LookAtRotation = FRotationMatrix::MakeFromX(Direction).Rotator();
+	HealthBarComponent->SetWorldRotation(LookAtRotation);
+
+	// 거리 기반 스케일 ⭐ (여기 같이 처리)
+	float Distance = FVector::Dist(CameraLocation, WidgetLocation);
+
+	float Scale = FMath::Clamp(Distance / 500.f, 0.5f, 1.5f);
+
+	HealthBarComponent->SetWorldScale3D(FVector(Scale));
+}
+
+void AZSMonsterBase::BaseDelegate()
+{
+	AbilitySystemComponent
+		->GetGameplayAttributeValueChangeDelegate(AttributeSet->GetHealthAttribute())
+		.AddUObject(this, &AZSMonsterBase::OnHealthChanged);
+}
+
+void AZSMonsterBase::BaseUI()
+{
+	if (HealthBarComponent)
+	{
+		UZS_MonsterHealthWidget* Widget =
+			Cast<UZS_MonsterHealthWidget>(HealthBarComponent->GetUserWidgetObject());
+
+		HealthBarWidget = Widget;
+	}
+
+	GetWorldTimerManager().SetTimer(
+		RotateHandle,
+		this,
+		&AZSMonsterBase::UpdateHealthBarRotation,
+		0.05f,
+		true
+	);
+}
+
+void AZSMonsterBase::UpdateUI()
+{
+	if (HealthBarWidget)
+	{
+		HealthBarComponent->SetVisibility(false);
+
+		float HP = AbilitySystemComponent->GetNumericAttribute(UZSAttributeSet::GetHealthAttribute());
+		float MaxHP = AbilitySystemComponent->GetNumericAttribute(UZSAttributeSet::GetMaxHealthAttribute());
+
+		float Ratio = HP / MaxHP;
+		HealthBarWidget->SetHealthPercent(Ratio);
+	}
+}
+
+void AZSMonsterBase::UpdateDissolve(float DeltaTime)
+{
+	DissolveValue += DeltaTime;
+
+	float Value = FMath::Clamp(DissolveValue / 2.0f, 0.f, 1.f);
+	
+	// UE_LOG(LogTemp, Error, TEXT("MID_Value:%.2f"),Value);
+	for (auto MID : DissolveMIDs)
+	{
+		MID->SetScalarParameterValue("DissolveAmount", Value);
+	}
+
+	if (Value >= 1.f)
+	{
+		Destroy();
+	}
+}
+
+void AZSMonsterBase::SetDissolve(bool ArgDissolove)
+{
+	bIsDissolving = ArgDissolove;
+}
+
+void AZSMonsterBase::InitializePlayerDA()
+{
+	if (!HasAuthority())
+		return;
+	if (!AbilitySystemComponent)
+		return;
+	UZSPlayerDataAsset* PlayerDA = CharacterData.LoadSynchronous();
+	if (!PlayerDA)
+		return;
+
+	GrantDefaultGA(PlayerDA);
+	ApplyDefaultAttributes(PlayerDA);
+
+}
+
+void AZSMonsterBase::GrantDefaultGA(UZSPlayerDataAsset* Data)
+{
+	if (!Data || !AbilitySystemComponent)
+	{
+		return;
+	}
+	
+	int32 InputID = 0;
+	for (const TSubclassOf<UGameplayAbility>& AbilityClass : CharacterData->StartupGA)
+	{
+		if (AbilityClass)
+		{
+			AbilitySystemComponent->GiveAbility(
+				FGameplayAbilitySpec(AbilityClass, 1, InputID, this));
+			InputID++;
+		}
+	}
+
+	FGameplayEffectContextHandle Ctx = AbilitySystemComponent->MakeEffectContext();
+	for (const TSubclassOf<UGameplayEffect>& GEClass : CharacterData->StartupGE)
+	{
+		if (GEClass)
+		{
+			AbilitySystemComponent->ApplyGameplayEffectToSelf(
+				GEClass->GetDefaultObject<UGameplayEffect>(),
+				1.f,
+				Ctx
+			);
+		}
+	}
+}
+
+void AZSMonsterBase::ApplyDefaultAttributes(UZSPlayerDataAsset* Data)
+{
+	if (!Data || !AttributeSet)
+	{
+		return;
+	}
+
+	AttributeSet->SetHealth(Data->Health);
+	AttributeSet->SetMaxHealth(Data->MaxHealth);
+
+	AttributeSet->SetStamina(Data->Stamina);
+	AttributeSet->SetMaxStamina(Data->MaxStamina);
+
+	AttributeSet->SetAttack(Data->Attack);
+	AttributeSet->SetArmor(Data->Armor);
+
+	AttributeSet->SetSpeed(Data->Speed);
+}
+
+void AZSMonsterBase::OnHealthChanged(const FOnAttributeChangeData& Data)
+{
+	float NewHealth = Data.NewValue;
+	float MaxHealth = AttributeSet->GetMaxHealth();
+
+	float Ratio = NewHealth / MaxHealth;
+
+	if (HealthBarWidget)
+	{
+		HealthBarWidget->SetHealthPercent(Ratio);
+	}
+
+	if (HealthBarComponent && !HealthBarComponent->IsVisible())
+	{
+		HealthBarComponent->SetVisibility(true);
+	}
+
+	// if (NewHealth <= 0.f)
+	// {
+	// 	Die();
+	// }
+}
+
+void AZSMonsterBase::ApplyDissolveMI()
+{
+	DissolveMIDs.Empty();
+
+	// for (int i = 0; i < GetMesh()->GetNumMaterials(); i++)
+	// {
+	// 	UMaterialInstanceDynamic* MID =
+	// 		GetMesh()->CreateAndSetMaterialInstanceDynamic(i);
+	//
+	// 	DissolveMIDs.Add(MID);
+	// }
+
+	if (!DissolveMaterial)
+	{
+		UE_LOG(LogTemp, Error, TEXT("DissolveMaterial NULL"));
+		return;
+	}
+
+	for (int i = 0; i < GetMesh()->GetNumMaterials(); i++)
+	{
+		UMaterialInstanceDynamic* MID =
+			UMaterialInstanceDynamic::Create(DissolveMaterial, this);
+
+		GetMesh()->SetMaterial(i, MID); // ⭐ 이게 핵심
+
+		DissolveMIDs.Add(MID);
+	}
+		
+}
--- Source/ZS/Character/Monster/ZSMonsterBase.h
+++ Source/ZS/Character/Monster/ZSMonsterBase.h
@@ -8,7 +8,9 @@
 #include "GAS/Attribute/ZSAttributeSet.h"
 #include "GameFramework/Character.h"
 #include "ZSMonsterBase.generated.h"
-
+class UZSPlayerDataAsset;
+class UWidgetComponent;
+class UZS_MonsterHealthWidget;
 UCLASS()
 class ZS_API AZSMonsterBase : public ACharacter, public IAbilitySystemInterface
 {
@@ -28,27 +30,76 @@ public:
 
 	// Called to bind functionality to input
 	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
-
+	virtual void GetLifetimeReplicatedProps(TArray<FLifetimeProperty>& OutLifetimeProps) const override;
 	virtual UAbilitySystemComponent* GetAbilitySystemComponent() const override;
 
+	void DeathMonster();
 	void MoveToNextPoint();
 	void SwitchTarget();
 	void Set_StartTargetPoint(ATargetPoint* ParaTarget);
 	void Set_EndTargetPoint(ATargetPoint* ParaTarget);
 
 protected:
+	void UpdateHealthBarRotation();
+	void BaseDelegate();
+	void BaseUI();
+	void UpdateUI();
+
+	FTimerHandle RotateHandle;
+
+#pragma region EFFECT
+	void UpdateDissolve(float DeltaTime);
+	void ApplyDissolveMI();
+	void SetDissolve(bool ArgDissolove = true);
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly)
+	UMaterialInterface* DissolveMaterial;
+	
+	UPROPERTY()
+	TArray<UMaterialInstanceDynamic*> DissolveMIDs;
+
+	float DissolveValue = 0.f;
+	bool bIsDissolving = false;
+	
+#pragma endregion
+
+	
+#pragma region  GAS
+
+	
+	void InitializePlayerDA();
+	void GrantDefaultGA(UZSPlayerDataAsset* Data);
+	void ApplyDefaultAttributes(UZSPlayerDataAsset* Data);
+
+	UPROPERTY(Replicated, EditDefaultsOnly, BlueprintReadOnly, Category = "SK|GAS")
+	TSoftObjectPtr<UZSPlayerDataAsset> CharacterData;
+	
 	UPROPERTY()
 	UAbilitySystemComponent* AbilitySystemComponent;
 
 	UPROPERTY()
 	UZSAttributeSet* AttributeSet;
 
+	#pragma endregion 
+	
 	UPROPERTY(EditAnywhere, Category = "AI")
 	ATargetPoint* StartTargetPoint;
 
 	UPROPERTY(EditAnywhere, Category = "AI")
 	ATargetPoint* EndTargetPoint;
 
+	UPROPERTY(VisibleAnywhere)
+	UWidgetComponent* HealthBarComponent;
 
+	UPROPERTY()
+	UZS_MonsterHealthWidget* HealthBarWidget;
+	
+
+	
 	int32 CurrentIndex = 0;
+
+#pragma region Delegate
+	void OnHealthChanged(const FOnAttributeChangeData& Data);
+#pragma endregion 
 };
+
+
