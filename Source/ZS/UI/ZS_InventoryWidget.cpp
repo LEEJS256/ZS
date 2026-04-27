7798daa1a8b9e85f98e5579bda07473ae753cde5
LEEJS256 <68937552+LEEJS256@users.noreply.github.com>
2026-04-28 04:56:05 +0900

    fix(UI): 인벤토리 유아이 추가

--- Source/ZS/UI/ZS_InventoryWidget.cpp
+++ Source/ZS/UI/ZS_InventoryWidget.cpp
@@ -3,3 +3,74 @@
 
 #include "UI/ZS_InventoryWidget.h"
 
+#include "ZS_InventorySlot.h"
+#include "Components/UniformGridPanel.h"
+#include "Components/UniformGridSlot.h"
+
+void UZS_InventoryWidget::NativeConstruct()
+{
+	Super::NativeConstruct();
+
+	ItemGrid.SetNum(Grid_Num * Grid_Num);
+
+	if (!GridPanel || !SlotClass)
+		return;
+
+}
+
+void UZS_InventoryWidget::NativePreConstruct()
+{
+	Super::NativePreConstruct();
+	RefreshGrid();
+}
+
+void UZS_InventoryWidget::RefreshGrid()
+{
+	GridPanel->ClearChildren(); 
+	MakeGrid();
+}
+
+void UZS_InventoryWidget::SynchronizeProperties()
+{
+	Super::SynchronizeProperties();
+
+	if (!GridPanel || !SlotClass) return;
+	
+	if (Grid_Num <= 0) 
+	{
+		GridPanel->ClearChildren();
+		return;
+	}
+	
+	GridPanel->ClearChildren(); 
+	MakeGrid();
+}
+
+void UZS_InventoryWidget::MakeGrid()
+{
+	for (int32 Y = 0; Y < Grid_Num; Y++)
+	{
+		for (int32 X = 0; X < Grid_Num; X++)
+		{
+			UZS_InventorySlot* newInventorySlot = CreateWidget<UZS_InventorySlot>(GetWorld(), SlotClass);
+
+			if (!newInventorySlot)
+				return;
+
+			newInventorySlot->SetIndex(X, Y);
+
+			//UGridSlot* GridSlot = GridPanel->AddChildToGrid(newInventorySlot,Y,X);
+
+			UUniformGridSlot* GridSlot = GridPanel->AddChildToUniformGrid(newInventorySlot, Y, X);
+
+
+			if (GridSlot)
+			{
+				// GridSlot->SetPadding((FMargin(0.f)));
+
+				GridSlot->SetHorizontalAlignment(HAlign_Fill);
+				GridSlot->SetVerticalAlignment(VAlign_Fill);
+			}
+		}
+	}
+}
--- Source/ZS/UI/ZS_InventoryWidget.h
+++ Source/ZS/UI/ZS_InventoryWidget.h
@@ -9,9 +9,47 @@
 /**
  * 
  */
+class UZS_InventoryWidget;
+
+USTRUCT(BlueprintType)
+struct FGridCell
+{
+	GENERATED_BODY()
+
+	bool bOccupied = false;
+	// class UZS_ItemData* Item;
+
+	UPROPERTY()
+	TObjectPtr<class UZS_ItemData> Item = nullptr;
+};
+
 UCLASS()
 class ZS_API UZS_InventoryWidget : public UUserWidget
 {
 	GENERATED_BODY()
+
+public:
+	void NativeConstruct() override;
+
+	void NativePreConstruct() override;
+
+	void RefreshGrid();
+	UPROPERTY()
+	TArray<FGridCell> ItemGrid;
+
+	virtual void SynchronizeProperties() override;
+private:
+	void MakeGrid();
+protected:
+	UPROPERTY(EditDefaultsOnly, BlueprintReadWrite, Category = "Inventory")
+	int32  Grid_Num = 0;
 	
+	// UPROPERTY(meta = (BindWidget))
+	// class UGridPanel* GridPanel;
+
+	UPROPERTY(meta = (BindWidget))
+	class UUniformGridPanel* GridPanel;
+	
+	UPROPERTY(EditAnywhere)
+	TSubclassOf<class UZS_InventorySlot> SlotClass;
 };
