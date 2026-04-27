d404e168b90e789ed364edf91e013b0f7df1fcc9
LEEJS256 <68937552+LEEJS256@users.noreply.github.com>
2026-04-28 04:31:23 +0900

    fix(DA) : Item DA추가

---
+++ Source/ZS/DA/ZS_ItemData.h
@@ -0,0 +1,32 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Engine/DataAsset.h"
+#include "ZS_ItemData.generated.h"
+
+/**
+ * 
+ */
+// class UPaperSprite;
+UCLASS()
+class ZS_API UZS_ItemData : public UDataAsset
+{
+	GENERATED_BODY()
+public:
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly)
+	FName ItemName;
+	//
+
+	// UPROPERTY(EditAnywhere, BlueprintReadOnly)
+	// UPaperSprite* ItemIcon_Sprite;
+	UPROPERTY(EditAnywhere, BlueprintReadOnly)
+	UTexture2D* ITemIcon;
+
+	// 핵심
+	UPROPERTY(EditAnywhere, BlueprintReadOnly)
+	TArray<FIntPoint> ShapeOffsets;
+	
+};
