3d50065da58de361e6f6553cc1c5cb44e992caa4
LEEJS256 <68937552+LEEJS256@users.noreply.github.com>
2026-05-05 03:40:57 +0900

    feat(Component) : 인벤토리 컴포넌트 추가

---
+++ Source/ZS/Component/ZS_InventoryComponent.cpp
@@ -0,0 +1,110 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Component/ZS_InventoryComponent.h"
+
+#include "DA/ZS_ItemData.h"
+
+// Sets default values for this component's properties
+UZS_InventoryComponent::UZS_InventoryComponent()
+{
+	// Set this component to be initialized when the game starts, and to be ticked every frame.  You can turn these features
+	// off to improve performance if you don't need them.
+	PrimaryComponentTick.bCanEverTick = false;
+
+	// ...
+}
+
+bool UZS_InventoryComponent::AddItem(UZS_ItemData* ItemData)
+{
+	for (int32 y = 0; y < GridHeight; y++)
+	{
+		for (int32 x = 0; x < GridWidth; x++)
+		{
+			if (PlaceItem(ItemData, FIntPoint(x, y)))
+			{
+				return true;
+			}
+		}
+	}
+
+	return false; // 실패
+}
+
+bool UZS_InventoryComponent::PlaceItem(UZS_ItemData* ItemData, FIntPoint Origin)
+{
+	if (!CanPlaceItem(ItemData, Origin))
+		return false;
+
+	FInventoryItem_Component NewItem;
+	NewItem.ItemData = ItemData;
+	NewItem.Origin = Origin;
+
+	int32 NewIndex = Items.Num();
+
+	for (const FIntPoint& Offset : ItemData->ShapeOffsets)
+	{
+		FIntPoint Cell = Origin + Offset;
+		int32 GridIndex = Cell.Y * GridWidth + Cell.X;
+
+		ItemGrid[GridIndex].bOccupied = true;
+		ItemGrid[GridIndex].ItemIndex = NewIndex;
+
+		NewItem.OccupiedCells.Add(Cell);
+	}
+
+	Items.Add(NewItem);
+
+	return true;
+}
+
+bool UZS_InventoryComponent::CanPlaceItem(UZS_ItemData* ItemData, FIntPoint Origin)
+{
+	for (const FIntPoint& Offset : ItemData->ShapeOffsets)
+	{
+		FIntPoint Cell = Origin + Offset;
+
+		if (Cell.X < 0 || Cell.X >= GridWidth ||
+			Cell.Y < 0 || Cell.Y >= GridHeight)
+		{
+			return false;
+		}
+
+		int32 Index = Cell.Y * GridWidth + Cell.X;
+
+		if (ItemGrid[Index].bOccupied)
+			return false;
+	}
+
+	return true;
+}
+
+void UZS_InventoryComponent::ClearGrid()
+{
+	for (FGridCell_Component& Cell : ItemGrid)
+	{
+		Cell.bOccupied = false;
+		Cell.ItemIndex = INDEX_NONE;
+	}
+}
+
+
+// Called when the game starts
+void UZS_InventoryComponent::BeginPlay()
+{
+	Super::BeginPlay();
+
+	ItemGrid.SetNum(GridWidth * GridHeight);
+	// ...
+	
+}
+
+
+// Called every frame
+void UZS_InventoryComponent::TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction)
+{
+	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
+
+	// ...
+}
+
---
+++ Source/ZS/Component/ZS_InventoryComponent.h
@@ -0,0 +1,86 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Components/ActorComponent.h"
+#include "ZS_InventoryComponent.generated.h"
+
+class UZS_ItemData;
+
+#pragma  region Struct
+
+USTRUCT(BlueprintType)
+struct FInventoryItem_Component
+{
+	GENERATED_BODY()
+
+	UPROPERTY()
+	TObjectPtr<UZS_ItemData> ItemData;
+
+	UPROPERTY()
+	int32 Count = 1;
+
+	UPROPERTY()
+	FIntPoint Origin;
+
+	UPROPERTY()
+	TArray<FIntPoint> OccupiedCells;
+};
+
+USTRUCT()
+struct FGridCell_Component
+{
+	GENERATED_BODY()
+
+	bool bOccupied = false;
+
+	UPROPERTY()
+	int32 ItemIndex = INDEX_NONE;
+};
+
+#pragma  endregion 
+
+UCLASS( ClassGroup=(Custom), meta=(BlueprintSpawnableComponent) )
+class ZS_API UZS_InventoryComponent : public UActorComponent
+{
+	GENERATED_BODY()
+
+public:	
+	// Sets default values for this component's properties
+	UZS_InventoryComponent();
+
+	bool AddItem(UZS_ItemData* ItemData);
+	bool PlaceItem(UZS_ItemData* ItemData, FIntPoint Origin);
+	bool CanPlaceItem(UZS_ItemData* ItemData, FIntPoint Origin);
+
+	void ClearGrid();
+
+	// Getter
+	const TArray<FInventoryItem_Component>& GetItems() const { return Items; }
+	const TArray<FGridCell_Component>& GetGrid() const { return ItemGrid; }
+	
+protected:
+	// Called when the game starts
+	virtual void BeginPlay() override;
+
+public:	
+	// Called every frame
+	virtual void TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction) override;
+
+private:
+	UPROPERTY()
+	TArray<FInventoryItem_Component> Items;
+
+	UPROPERTY()
+	TArray<FGridCell_Component> ItemGrid;
+
+public:
+
+	UPROPERTY(EditAnywhere, Category="Inventory")
+	int32 GridWidth = 5;
+
+	UPROPERTY(EditAnywhere, Category="Inventory")
+	int32 GridHeight = 8;
+		
+};
