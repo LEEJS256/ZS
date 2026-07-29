5a5e485f31ba3e1920bc8f6dbf2967392228e68f
LEEJS256 <68937552+LEEJS256@users.noreply.github.com>
2026-07-30 03:17:38 +0900

    feat(UI): 미니맵관련 유아이 추가

---
+++ Source/ZS/UI/ZS_MinimapUI.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Blueprint/UserWidget.h"
+#include "ZS_MinimapUI.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class ZS_API UZS_MinimapUI : public UUserWidget
+{
+	GENERATED_BODY()
+	
+};
